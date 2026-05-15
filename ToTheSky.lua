36bbb8c24378c844bb8df7c4da6f7ecb1298a817
QiumuBai <aku150lty@gmail.com>
2026-05-14 17:08:38 -0700

    Update ToTheSky.lua

--- ToTheSky.lua
+++ ToTheSky.lua
@@ -4,9 +4,9 @@ local Pid = {}
 function Pid.createPid(kp, ki, kd, tick, u)
     local pid = {
         u = u or 0,
-        e0 = 0., -- Current error
-        e1 = 0., -- Previous error
-        e2 = 0.  -- Error before previous
+        e0 = 0.0, -- Current error (Changed from 0. to 0.0)
+        e1 = 0.0, -- Previous error (Changed from 0. to 0.0)
+        e2 = 0.0  -- Error before previous (Changed from 0. to 0.0)
     }
 
     function pid:step(err)
@@ -16,7 +16,7 @@ function Pid.createPid(kp, ki, kd, tick, u)
         self.e0 = err
 
         -- Incremental PID Algorithm
-        -- Calculates the CHANGE (du) needed based on the slope of the error
+        -- du = P_term + I_term + D_term
         local du = kp * (self.e0 - self.e1) +
                    ki * tick * self.e0 +
                    kd * (self.e0 - 2 * self.e1 + self.e2) / tick
