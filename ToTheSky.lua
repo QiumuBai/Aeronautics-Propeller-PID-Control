240696e5b0389d7cd7e3042fe85d758dae90a22f
QiumuBai <aku150lty@gmail.com>
2026-05-14 21:38:57 -0700

    Update ToTheSky.lua

--- ToTheSky.lua
+++ ToTheSky.lua
@@ -4,9 +4,9 @@ local Pid = {}
 function Pid.createPid(kp, ki, kd, tick, u)
     local pid = {
         u = u or 0,
-        e0 = 0.0, -- Current error (Changed from 0. to 0.0)
-        e1 = 0.0, -- Previous error (Changed from 0. to 0.0)
-        e2 = 0.0  -- Error before previous (Changed from 0. to 0.0)
+        e0 = 0.0,
+        e1 = 0.0,
+        e2 = 0.0
     }
 
     function pid:step(err)
